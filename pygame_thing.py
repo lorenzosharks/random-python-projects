53ab64bab39ae2411531f22ca784bf359abcaffd
lorenzosharks <karsonx27@lakesideschool.org>
2024-05-20 20:53:30 -0700

    bruh

--- pygame_thing.py
+++ pygame_thing.py
@@ -19,7 +19,7 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
-
+    pygame.draw.rect()
     pygame.draw.circle(screen, "red", player_pos, 40)
 
     keys = pygame.key.get_pressed()
@@ -39,5 +39,4 @@ while running:
     # dt is delta time in seconds since last frame, used for framer-
     # independent physics.
     dt = clock.tick(60) / 1000
-
 pygame.quit()
